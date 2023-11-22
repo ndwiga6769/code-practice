8723516c5835db956401e35fcf8c3f954417fdeb
danwycliff <danwycliff6769@gmail.com>
2023-11-22 22:48:04 +0300

    codes

--- Valid Braces.py
+++ Valid Braces.py
@@ -13,3 +13,20 @@
 # "(}"       =>  False
 # "[(])"     =>  False
 # "[({})](]" =>  False
+
+
+def valid_braces(s):
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping.keys():
+            if not stack or stack[-1] != mapping[char]:
+                return False
+            stack.pop()
+        else:
+            return False
+
+    return not stack
