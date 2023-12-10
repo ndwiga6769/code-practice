47445bc71b4026725f0c6f29c6f18f0ad69bf8b3
danwycliff <danwycliff6769@gmail.com>
2023-12-10 19:44:07 +0300

    codes

---
+++ Simple validation of a username with regex.py
@@ -0,0 +1,12 @@
+# Write a simple regex to validate a username. Allowed characters are:
+
+# lowercase letters,
+# numbers,
+# underscore
+# Length should be between 4 and 16 characters (both included).
+
+import re
+
+def validate_username(username):
+    pattern = r'^[a-z0-9_]{4,16}$'
+    return bool(re.match(pattern, username))
