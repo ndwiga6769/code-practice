b3bf205e452c1f4d587d5b9f3f94f60934322095
danwycliff <danwycliff6769@gmail.com>
2024-01-06 17:54:22 +0300

    codes

--- Age Range Compatibility Equation.py
+++ Age Range Compatibility Equation.py
@@ -6,13 +6,15 @@
 # minimum age <= your age <= maximum age
 
 # Task
-# Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.
+# Given an integer (1 <= n <= 100) representing
+#  a person's age, return their minimum and maximum age range.
 
 # This equation doesn't work when the age <= 14, so use this equation instead:
 
 # min = age - 0.10 * age
 # max = age + 0.10 * age
-# You should floor all your answers so that an integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
+# You should floor all your answers so that an 
+# integer is given instead of a float (which doesn't represent age). Return your answer in the form [min]-[max]
 
 # ##Examples:
 
