2701e88ce83235fb68d6fac6695c8ef05c0077fb
ndwiga6769 <your.email@example.com>
2024-10-24 17:50:40 +0300

    codes

---
+++ adding.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace addition
+{
+class Program
+    {
+    static void Main(string[] args)
+    	{
+        int x = 3;
+       	int y = 6;
+        int z = x + y;
+        
+        Console.WriteLine(z);
+        }
+    }
+}
